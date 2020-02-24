8b61188f179fa55fb00780487d9b65c88573a75e
Maor <maorfeldinger@gmail.com>
2020-02-24 11:51:51 -0800

    add recipe-edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { SelectRecipeComponent } from './recipes/select-recipe/select-recipe.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 const appRoutes:Routes = [
   {path: '', redirectTo: 'recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent},
+  {path: 'recipes', component: RecipesComponent, children:[
+    {path: '', component: SelectRecipeComponent},
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailsComponent},
+    {path: ':id/edit', component: RecipeEditComponent}
+  ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]
 
---
+++ src/app/recipes/select-recipe/select-recipe.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-recipe',
+  templateUrl: './select-recipe.component.html'
+})
+
+export class SelectRecipeComponent implements OnInit {
+
+  ngOnInit(){
+
+  }
+}
