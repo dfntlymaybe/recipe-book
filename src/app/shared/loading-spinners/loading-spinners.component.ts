d0aa830f658307bab4ba8b1ff60a0e70ed834f92
Maor <maorfeldinger@gmail.com>
2020-03-10 19:47:58 -0700

    Add loading spinner component

---
+++ src/app/shared/loading-spinners/loading-spinners.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'loading-spinner',
+    templateUrl: './loading-spinners.component.html',
+    styleUrls: ['./loading-spinners.component.css']
+})
+
+export class LoadingSpinnerComponent {
+
+}
