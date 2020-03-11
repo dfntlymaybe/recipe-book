8e8f081ecf76ac3cf193e19ab23b14d43b8c1576
Maor <maorfeldinger@gmail.com>
2020-03-10 19:51:10 -0700

    Add auth and error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs";
+
+import { AuthService } from "./auth.service";
+import { AuthResponse } from "./auth-response.model";
 
 @Component({
     selector: 'app-auth',
@@ -9,12 +13,35 @@ import { NgForm } from "@angular/forms";
 
 export class AuthComponent implements OnInit{
   isLoginMode = true;
+  isLoading = false;
+  errorMessage: string = null;
 
+  constructor(private authService: AuthService){}
   onSwitchMode():void{
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(form: NgForm){
-    console.log(form.value)
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    let resObj: Observable<AuthResponse>;
+    this.isLoading = true;
+
+    if(this.isLoginMode){
+      resObj = this.authService.signIn(email, password);
+    }else{ //sign up mode
+      resObj = this.authService.signUp(email, password);
+    }
+    resObj.subscribe(response => {
+        console.log(response);
+        this.isLoading = false;
+    }, errorMessage => {
+        this.errorMessage = errorMessage;
+        console.log(errorMessage);
+        this.isLoading = false;
+    });
     form.reset();
   }
   ngOnInit(){
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,50 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+import { env } from "../../../.env";
+import { AuthResponse } from "./auth-response.model";
 
 @Injectable({providedIn: "root"})
 export class AuthService {
-  
+
+  constructor(private http: HttpClient){}
+
+  signUp(email: string, password: string): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(
+      env.signUpUrl,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }).pipe(catchError(this.handleError))
+  }
+  signIn(email: string, password: string): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(
+      env.signInUrl,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }).pipe(catchError(this.handleError))
+  }
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = "An Error Occurred!"
+    if(!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch(errorRes.error.error.message){
+      case "EMAIL_EXISTS":
+        errorMessage = "This email already exists!";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "email NOT found";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "Wrong password";
+        break;
+    }
+    return throwError(errorMessage);
+  }
 }
